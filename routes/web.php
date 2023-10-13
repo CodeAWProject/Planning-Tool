1b536c7adb01ef70c6f173a1d665b1b9218659a2
CodeAWProject <activityware@gmail.com>
2023-10-13 13:35:10 +0200

    Route model binding is utilized to retrieve a specific task by its unique identifier, eliminating the need for further database initialization

--- routes/web.php
+++ routes/web.php
@@ -29,18 +29,18 @@ Route::get('/tasks', function () {
 Route::view('tasks/create', 'create')
 ->name('tasks.create');
 
-Route::get('/tasks/{id}', function ($id)  {
+Route::get('/tasks/{task}', function (Task $task)  {
   
     return view('show', [
-      'task' => Task::findOrFail($id)
+      'task' => $task
     ]);
 })->name('tasks.show');
 
 
-Route::get('/tasks/{id}/edit', function ($id)  {
+Route::get('/tasks/{task}/edit', function (Task $task)  {
   
   return view('edit', [
-    'task' => Task::findOrFail($id)
+    'task' => $task
   ]);
 })->name('tasks.edit');
 
@@ -67,14 +67,13 @@ Route::post('/tasks', function (Request $request) {
 
 
 
-Route::put('/tasks/{id}', function ($id, Request $request) {
+Route::put('/tasks/{task}', function (Task $task, Request $request) {
   $data = $request->validate([
     'title' => 'required|max:255',
     'description' => 'required',
     'long_description' => 'required'
   ]);
 
-  $task = Task::findOrFail($id);
   $task->title = $data['title'];
   $task->description = $data['description'];
   $task->long_description = $data['long_description'];
