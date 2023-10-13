b147fe0ea5014f1ce1171c7d5b74b4f7d4883a75
CodeAWProject <activityware@gmail.com>
2023-10-13 14:19:18 +0200

    Mass assignment allows you to efficiently populate multiple data fields, enabling streamlined querying, all within a single code line. However, it is essential to ensure that these fields are appropriately added to the model's 'fillable' attributes

--- routes/web.php
+++ routes/web.php
@@ -49,15 +49,17 @@ Route::get('/tasks/{task}/edit', function (Task $task)  {
 
 Route::post('/tasks', function (TaskRequest $request) {
  
-  $data = $request->validated();
-  $task = new Task;
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-  $task->save();
+  // $data = $request->validated();
+  // $task = new Task;
+  // $task->title = $data['title'];
+  // $task->description = $data['description'];
+  // $task->long_description = $data['long_description'];
+  // $task->save();
+
+  $task = Task::create($request->validated());
 
 
-  return redirect()->route('tasks.show', ['id' => $task->id])
+  return redirect()->route('tasks.show', ['task' => $task->id])
   ->with('success', 'Task created successfully!');
 
 })->name('tasks.store');
@@ -67,14 +69,16 @@ Route::post('/tasks', function (TaskRequest $request) {
 Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
  
 
-  $data = $request->validated();
-  $task->title = $data['title'];
-  $task->description = $data['description'];
-  $task->long_description = $data['long_description'];
-  $task->save();
+  //$data = $request->validated();
+  // $task->title = $data['title'];
+  // $task->description = $data['description'];
+  // $task->long_description = $data['long_description'];
+  // $task->save();
+
+  $task->update($request->validated());
 
 
-  return redirect()->route('tasks.show', ['id' => $task->id])
+  return redirect()->route('tasks.show', ['task' => $task->id])
   ->with('success', 'Task updated successfully!');
 
 })->name('tasks.update');
